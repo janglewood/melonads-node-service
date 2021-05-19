43dc137308d4dbd808e2d223acb8931c16e49967
Anton <zhl96@mail.ru>
2021-05-19 10:22:08 +0300

    FEAT: initial commit

---
+++ email.js
@@ -0,0 +1,48 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+
+const { headersSettingsMidleware } = require('./midlewares');
+
+const app = express();
+app.use(bodyParser.urlencoded(), bodyParser.json(), headersSettingsMidleware);
+const contactAddress = 'test@api.wantbox.by';
+const transport = {
+  sendmail: true,
+  newline: 'unix',
+  path: '/usr/sbin/sendmail',
+  host: 'smtps://api.wantbox.by',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'test@api.wantbox.by',
+    pass: 'p9u8t7I6n5!',
+  },
+  tls: {
+    rejectUnauthorized: false, // if on local
+  },
+};
+
+const mailer = nodemailer.createTransport(transport);
+app.post('/order', cors(), (req, res) => {
+  const { name, message, phoneNumber, subject, email } = req.body;
+  mailer.sendMail(
+    {
+      from: email,
+      to: [contactAddress],
+      subject: subject || '[No subject]',
+      html:
+        `<div><h1>Привет, ${name}! Это твой номер - ${phoneNumber}?</h1><span style="color: red">${message}</span></div>` ||
+        '[No message]',
+      // html: `<div>${message}</div>`,
+    },
+    function (err, info) {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      res.json({ success: true, info });
+    }
+  );
+});
+app.listen(3003);
---
+++ midlewares.js
@@ -0,0 +1,12 @@
+const headersSettingsMidleware = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+};
+
+module.exports = {
+  headersSettingsMidleware,
+};
